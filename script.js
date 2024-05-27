a4664bcf6063d8003b1466d9c07529c36c1f35d9
Rachel Pizane <rachel.pzm@gmail.com>
2024-05-27 18:23:54 -0300

    Finalizei a parte de HTML e corrigi algumas funções no Javascrip

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ let btnMain = document.getElementById("btnMain");
 let firstCard = document.getElementById("first-card");
 let secondCard = document.getElementById("second-card");
 let arrowBack = document.getElementById("arrowBack")
+let numRatingBtn;
 
 function addRating(numRating){
     chosenRating.innerHTML = numRating.innerHTML
+
+    if( numRatingBtn){
+        numRatingBtn.classList.remove("card-btn-select")
+    }
+        
+    numRatingBtn = document.getElementById("btn" + numRating.innerHTML);
+    numRatingBtn.classList.add("card-btn-select")
 }
 
-
 btnMain.addEventListener("click", function(){
-    if(chosenRating.innerHTML !== ""){
+    if(chosenRating.innerHTML){
         firstCard.style.display = "none";
         secondCard.style.display = "grid";
     } else {
-        alert("You need to give a rating to move on to the next card!")
+        alert("Please select a rating before submitting.")
     }
 })
 
@@ -22,5 +29,5 @@ arrowBack.addEventListener("click", ()=>{
     firstCard.style.display = "grid";
     secondCard.style.display = "none";
     chosenRating.innerHTML = "";
-
+    numRatingBtn.classList.remove("card-btn-select")
 })
